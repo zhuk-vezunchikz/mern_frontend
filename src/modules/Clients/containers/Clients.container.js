fc181bee724b33868a2de26cb4d04bb5ba6346e1
Yauheni <evgeniy.zhuk.by@gmail.com>
2021-05-04 18:25:50 +0300

    [feature] - created post request

--- src/modules/Clients/containers/Clients.container.js
+++ src/modules/Clients/containers/Clients.container.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ClientsComponent from '../components/Clients.component';
 import { useSelector } from 'react-redux';
 
 const ClientsContainer = () => {
+
   const clients = useSelector((state) => state.clientsReducer.clients);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch('api/clients/addClient', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({firstName: 'Evgeniy', lastName: 'Zhuk', phone: "111 11 11"})
+      })
+      const result = await response.json()
+      console.log(result);
+    } catch(e) {
+      throw e
+    }
+    
+  }, [])
+
   return <ClientsComponent clients={clients} />;
 };
 
